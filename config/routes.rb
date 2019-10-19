069c423d0611329c7c7cc1931fb33c8dc272ff9b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-19 14:20:18 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
   devise_for :users
+  get 'dashboard', to: 'dashboard#index'
 end
