f19621ab696c0fa0e096516917af06cd91711322
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-22 13:54:28 +0900

    ywt の追加の実装

--- app/controllers/ywts_controller.rb
+++ app/controllers/ywts_controller.rb
@@ -1,7 +1,24 @@
 class YwtsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @ywt = Ywt.new
   end
 
   def create
+    @ywt = Ywt.new(ywt_params)
+    @ywt.user = current_user
+
+    if @ywt.save
+      redirect_to(controller: :dashboard, action: :index)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def ywt_params
+    params.require(:ywt).permit(:y, :w, :t)
   end
 end
