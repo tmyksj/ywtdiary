3dd4a59d33a6790d3ec25996a2144ac76bdc48a1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-22 14:03:55 +0900

    リファクタリング

--- app/models/ywt.rb
+++ app/models/ywt.rb
@@ -1,3 +1,3 @@
 class Ywt < ApplicationRecord
-  belongs_to :user
+  belongs_to(:user)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'home#index'
-  devise_for :users
-  get 'dashboard', to: 'dashboard#index'
+  root(to: 'home#index')
+  devise_for(:users)
+  get('dashboard', to: 'dashboard#index')
 end
