8ed85bbb4ebee548af731846c6f9862b5cb4076f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-22 13:50:48 +0900

    バリデーションを追加

--- app/models/ywt.rb
+++ app/models/ywt.rb
@@ -1,3 +1,7 @@
 class Ywt < ApplicationRecord
   belongs_to :user
+
+  validates :y, presence: true, length: { maximum: 250 }
+  validates :w, presence: true, length: { maximum: 250 }
+  validates :t, presence: true, length: { maximum: 250 }
 end
